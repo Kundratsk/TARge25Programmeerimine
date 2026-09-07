de5cce867c2cc752ab38bf073866938e98621aa2
Miilo2004 <48867030+Miilo2004@users.noreply.github.com>
2026-09-07 15:26:50 +0300

    Update project

---
+++ TARge25shop/TARge25shop.Data/TARge25ShopContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TARge25shop.Data
+{
+    //Nimetasime classi TARge25ShopContext, mis prärib DbContext klassi
+    public class TARge25ShopContext : DbContext
+    {
+        //All on konstruktor, mis pärib DbContext klassi
+        public TARge25ShopContext (DbContextOptions<TARge25ShopContext> options) : base(options){ }
+    }
+}
