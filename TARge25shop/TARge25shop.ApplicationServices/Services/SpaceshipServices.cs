7be1d662de3813ec03c4edd18d6c74b03cea640a
Miilo2004 <48867030+Miilo2004@users.noreply.github.com>
2026-09-07 16:17:14 +0300

    Spaceship domain

---
+++ TARge25shop/TARge25shop.ApplicationServices/Services/SpaceshipServices.cs
@@ -0,0 +1,18 @@
+﻿using TARge25shop.Core.Domain;
+
+
+namespace TARge25shop.ApplicationServices.Services
+{
+    public class SpaceshipServices
+    {
+        
+       public async Task<Spaceship> Create(SpaceshipDto dto)
+        {
+
+
+
+            return null;
+        }
+    }
+
+}
---
+++ TARge25shop/TARge25shop.Core/Domain/SpaceshipDto.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace TARge25shop.Core.Domain
+{
+    public class SpaceshipDto
+    {
+    }
+}
