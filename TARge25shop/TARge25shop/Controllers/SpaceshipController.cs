be83dfa1992dde67c6cdd18652afe7206b7e91f0
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-15 11:19:02 +0300

    Merge createupdate

--- TARge25shop/TARge25shop/Controllers/SpaceshipController.cs
+++ TARge25shop/TARge25shop/Controllers/SpaceshipController.cs
@@ -44,11 +44,13 @@ namespace TARge25shop.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            SpaceshipCreateUpdateViewModel result = new();
+
+            return View("CreateUpdate", result);
         }
         [HttpPost]
 
-        public async Task<IActionResult> Create(SpaceshipCreateViewModel vm)
+        public async Task<IActionResult> Create(SpaceshipCreateUpdateViewModel vm)
         {
             var dto = new SpaceshipDto
             {
@@ -81,7 +83,7 @@ namespace TARge25shop.Controllers
                 return NotFound();
             }
 
-            var vm = new SpaceshipUpdateViewModel
+            var vm = new SpaceshipCreateUpdateViewModel
             {
                 Id = spaceship.Id,
                 Name = spaceship.Name,
@@ -90,11 +92,11 @@ namespace TARge25shop.Controllers
                 EnginePower = spaceship.EnginePower
             };
 
-            return View(vm);
+            return View("CreateUpdate", vm);
         
         }
         [HttpPost]
-        public async Task<IActionResult> Update(SpaceshipUpdateViewModel vm)
+        public async Task<IActionResult> Update(SpaceshipCreateUpdateViewModel vm)
         {
             var dto = new SpaceshipDto
             {
