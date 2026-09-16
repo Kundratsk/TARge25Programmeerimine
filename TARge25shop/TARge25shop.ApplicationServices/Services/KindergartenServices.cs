d109472e23094a1d6f5635b7e97afdb0e730789d
Kundratsk <kundratsk@gmail.com>
2026-09-16 22:44:29 +0300

    Create files and make Kindergarten view

---
+++ TARge25shop/TARge25shop.ApplicationServices/Services/KindergartenServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TARge25shop.ApplicationServices.Services
+{
+    internal class KindergartenServices
+    {
+    }
+}
---
+++ TARge25shop/TARge25shop/Models/Kindergarten/KindergartenDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TARge25shop.Models.Kindergarten
+{
+    public class KindergartenDetailsViewModel
+    {
+        public Guid? Id { get; set; }
+        public string GroupName { get; set; } = string.Empty;
+        public string KindergartenName { get; set; } = string.Empty;
+
+        public int ChildrenCount { get; set; }
+        public string TeacherName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
